0720ffc597f54a7c43505191eb1240453e1c85db
Holden Wiese <wieseholden@gmail.com>
2019-11-21 15:21:42 -0700

    Add all exercise with JavaScript up to Step: 57.

--- Basic_JavaScript_Projects/resources/javascript/JavaScript_File.js
+++ Basic_JavaScript_Projects/resources/javascript/JavaScript_File.js
@@ -1,1 +1,39 @@
-alert("Hello World!");
+
+
+// document.write("Hello World!");
+
+
+
+// var Holden = "Hi there. My name is Holden. What\'s your name?";
+// document.write(Holden);
+// alert(Holden);
+
+
+
+// var bunny = "There once was a nice bunny. " +"It\'s kind demeanor made everyone happy. " + "Then, one day it disappeared.";
+// document.write(bunny);
+
+
+
+// var family = "Wiese", holden = "Holden", hadden = "Hadden", haley = "Haley", amy = "Amy", mom = "Louise", dad = "Matt";
+
+// var family = family.fontcolor("gold");
+// var holden = holden.fontcolor("red");
+// var hadden = hadden.fontcolor("blue"); 
+// var haley = haley.fontcolor('green'); 
+// var amy = amy.fontcolor('yellow');
+// var mom = mom.fontcolor('orange');
+// var dad = dad.fontcolor('purple');
+
+// document.write(family + holden + hadden + dad);
+
+
+
+// document.write(12+12);
+
+function practiceFunction() {
+    var str ="This text is green!";
+    var result = str.fontcolor('green');
+    document.getElementById("buttonClicked").innerHTML = result;
+}
+
